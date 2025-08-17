6f2a8fa951459d2aad19822a27303ee6172c3188
Anand Raut <anandraut7794@gmail.com>
2025-08-18 00:46:01 +0530

    seperated them lmao

--- server/main.py
+++ server/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from server import summarize
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+import smtplib
+from email.mime.text import MIMEText
 import os
 from dotenv import load_dotenv
 
@@ -12,32 +12,24 @@ load_dotenv()
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
+    CORSMiddleware, 
+    allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-class SummarizeRequest(BaseModel):
+class SummarizeRequest(BaseModel):  
     transcript: str
     instruction: str
 
-@app.get("/api/health")
-def index():
-    return {"status": "running"}
 
-@app.post("/api/summarize")
+@app.get("/")
+def index():
+    return {"IsRunning": True}
+
+@app.post("/summarize")
 def generate_summary(req: SummarizeRequest):
     summary = summarize(req.transcript, req.instruction)
     return {"summary": summary}
 
-
-dist_path = os.path.join(os.path.dirname(__file__), "..", "client", "dist")
-
-if os.path.isdir(dist_path):
-    app.mount("/assets", StaticFiles(directory=os.path.join(dist_path, "assets")), name="assets")
-
-    @app.get("/")
-    def serve_index():
-        return FileResponse(os.path.join(dist_path, "index.html"))
